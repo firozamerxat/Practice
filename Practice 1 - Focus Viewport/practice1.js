3798042f51c0ae540f35380df0f13d3d65eb11b2
Firoza Merxat <firozauxui@gmail.com>
2022-07-26 01:35:46 -0400

    Practice 1

---
+++ Practice 1 - Focus Viewport/practice1.js
@@ -0,0 +1,15 @@
+const container=document.querySelector('.container');
+const imgs=document.querySelectorAll ('.container img');
+
+container.addEventListener('mousemove', function(e){
+    let percent=e.clientX/Window.outerWide;
+    let offset=10*percent;
+    let blur=20;
+
+    imgs.forEach((img,index)=>{
+        offset*=1.3;
+        let blurValue=Math.pow(index/imgs.length-percent, 2)*blur;
+        img.style.setProperty('--offset',offset+'px');
+        img.style.setProperty('--blur',blurValue+'px');
+    })
+})
